4ccc2df36631eb5e4d10352b26835ebf59c84c37
AzureEtude <azureetude@hotmail.com>
2026-07-23 15:32:40 +0800

    feat:打包流程优化

--- tools/install.py
+++ tools/install.py
@@ -104,9 +104,13 @@ def install_resource():
     configure_ocr_model()
 
     shutil.copytree(
-        working_dir / "assets" ,
-        install_path / "assets",
+        working_dir / "assets" / "resource",
+        install_path / "resource",
         dirs_exist_ok=True,
+    )
+    shutil.copy2(
+        working_dir / "assets" / "interface.json",
+        install_path,
     )
 
     with open(install_path / "interface.json", "r", encoding="utf-8") as f:
